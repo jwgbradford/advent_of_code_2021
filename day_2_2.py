a19a747f731b39f48e9c2dfc70cc24b54a96290a
jwgbradford@gmail.com <jwgbradford@gmail.com>
2021-12-03 13:40:46 +0000

    tidied functions up

--- day_2_2.py
+++ day_2_2.py
@@ -1,31 +1,25 @@
 def read_file(file):
-    input_file = open(file, 'r')
-    input_data = input_file.read()
-    input_file.closed
+    with open(file, "r") as f:
+        input_data = f.readlines()
     return input_data
-
-def get_data(file):
-    file_data = read_file(file)
-    data_list = file_data.split("\n")
-    return data_list
 
 def split_commands(data_list):
     cmd_list = []
     for command in data_list:
         direction, distance = command.split(" ")
-        cmd_list.append((direction, distance))
+        cmd_list.append((direction, int(distance)))
     return cmd_list
 
 def find_position(movements):
     horizontal, depth, aim = 0, 0, 0
     for movement in movements:
         if movement[0] == 'forward':
-            horizontal += int(movement[1])
-            depth += aim * int(movement[1])
+            horizontal += movement[1]
+            depth += aim * movement[1]
         elif movement[0] == 'back':
-            horizontal -= int(movement[1])
+            horizontal -= movement[1]
         elif movement[0] == 'up':
-            aim -= int(movement[1])
+            aim -= movement[1]
         elif movement[0] == 'down':
             aim += int(movement[1])
     return (horizontal, depth)
@@ -34,7 +28,7 @@ def multiply_postion(final_pos):
     multi_pos = final_pos[0] * final_pos[1]
     return multi_pos
 
-my_list = get_data('day_2.txt')
+my_list = read_file('test2.txt')
 command_list = split_commands(my_list)
 final_position = find_position(command_list)
 answer = multiply_postion(final_position)
